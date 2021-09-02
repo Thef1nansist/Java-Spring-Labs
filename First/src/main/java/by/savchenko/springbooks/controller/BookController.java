3bf230a5510a02f959de03285277d77e068cdbaf
Thef1nansist <vladsavhchenko994@gmail.com>
2021-09-02 21:25:17 +0300

    Java LOL

--- First/src/main/java/by/savchenko/springbooks/controller/BookController.java
+++ First/src/main/java/by/savchenko/springbooks/controller/BookController.java
@@ -2,6 +2,7 @@ package by.savchenko.springbooks.controller;
 
 import by.savchenko.springbooks.forms.BookForm;
 import by.savchenko.springbooks.model.Book;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-
 @Controller
 public class BookController {
     private static List<Book> bookList = new ArrayList<>();
@@ -20,6 +18,7 @@ public class BookController {
         bookList.add(new Book("Full stack 1","Savchenko Vlad"));
         bookList.add(new Book("Full stack 2","Savchenko Vladick"));
     }
+    private static int ind = 0;
 
     @Value("${welcom.message}")
     private String message;
@@ -49,22 +48,6 @@ public class BookController {
         model.addAttribute("bookform", bookForm);
         return modelAndView;
     }
-
-    @RequestMapping(value = "/update/{book}", method = RequestMethod.GET)
-    public ModelAndView ShowUpdateListPage(@PathVariable(value = "book") Book book){
-        ModelAndView modelAndView = new ModelAndView("update");
-        BookForm bookForm = new BookForm();
-        bookForm.setAuthor(book.getAuthor());
-        book.setTitle(book.getTitle());
-        return modelAndView;
-    }
-    
-    @RequestMapping(value = "/update", method = RequestMethod.PUT)
-    public ModelAndView ActionUpdateProduct(Model model, @ModelAttribute("updateform") BookForm bookform){
-        ModelAndView modelAndView = new ModelAndView();
-        return modelAndView;
-    }
-
     @RequestMapping(value = "/addbook", method = RequestMethod.POST)
     public ModelAndView ActionAddListPage(Model model, @ModelAttribute("bookform") BookForm bookForm){
         ModelAndView modelAndView = new ModelAndView();
@@ -80,6 +63,32 @@ public class BookController {
         }
         model.addAttribute("errorMessage", errorMessage);
         modelAndView.setViewName("addbook");
+        return modelAndView;
+    }
+    @GetMapping("{update}")
+    public ModelAndView UpdateGetProduct(Model model, @ModelAttribute("productUpdate") Book book){
+        ModelAndView modelAndView = new ModelAndView("update");
+        BookForm bookForm = new BookForm();
+        bookForm.setTitle(book.getTitle());
+        bookForm.setAuthor(book.getAuthor());
+        ind  =bookList.indexOf(book);
+        model.addAttribute("bookform", bookForm);
+        return  modelAndView;
+    }
+
+    @PostMapping("{update}")
+    public ModelAndView UpdatePostProduct(Model model, @ModelAttribute("productUpdate") Book book){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("booklist");
+        String title = book.getTitle();
+        String author = book.getAuthor();
+        if(title != null && title.length()>0 && author != null && author.length()>0){
+            bookList.get(ind).setTitle(title);
+            bookList.get(ind).setAuthor(author);
+            model.addAttribute("books", bookList);
+            return  modelAndView;
+        }
+        model.addAttribute("bookList",bookList);
         return modelAndView;
     }
 
